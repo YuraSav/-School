cb8672d8d85477bc02106e93968e8e12cdf325a0
YuraSav <143741247+YuraSav@users.noreply.github.com>
2025-06-03 15:39:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+  $('.bxslider').bxSlider({
+    minSlides: 3,
+    maxSlides: 3,
+    slideWidth: 440,
+    slideMargin: 200
+  });
+
+  $('.article-reviews .bxslider').article-reviews .bxSlider({
+    minSlides2: 4,
+    maxSlides2: 4,
+    slideWidth2: 440,
+    slideMargin2: 90
+  });

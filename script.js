ce1c510e7a3b5f7d8b750dc746a08dd0d0d4ae35
YuraSav <143741247+YuraSav@users.noreply.github.com>
2025-06-15 20:38:41 +0300

    Add files via upload
    Добавлен 2-я страница!

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-  $('.bxslider').bxSlider({
+  $('.course__list').bxSlider({
     minSlides: 3,
     maxSlides: 3,
     slideWidth: 440,
     slideMargin: 200
   });
 
-  $('.article-reviews .bxslider').article-reviews .bxSlider({
-    minSlides2: 4,
-    maxSlides2: 4,
-    slideWidth2: 440,
-    slideMargin2: 90
+  $('.block__reviews').bxSlider({
+    minSlides: 3,
+    maxSlides: 3,
+    slideWidth: 440,
+    slideMargin: 200
   });
+
+   $('.work-list').bxSlider({
+    minSlides: 4,
+    maxSlides: 4,
+    slideWidth: 290,
+    slideMargin: 120
+  });
+
+  let directions = document.querySelector("#directions");
+  let links = document.querySelectorAll(".directions-link");
+  function opendirections(){
+    links.forEach(function(link){
+      link.style.transform="scale(1)";
+    })
+  }
+  directions.addEventListener("mouseover",opendirections)
+  function clousedirections(){
+    links.forEach(function(link){
+      link.style.transform="scale(0)";
+    })
+  }
+  directions.addEventListener("mouseout",clousedirections)

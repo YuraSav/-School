6163720a2324c2b656146292e1444b2c31b403c5
YuraSav <143741247+YuraSav@users.noreply.github.com>
2025-07-05 17:43:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-  $('.course__list').bxSlider({
+
+ $('.course__list').bxSlider({
     minSlides: 3,
     maxSlides: 3,
     slideWidth: 440,
     slideMargin: 200
   });
 
+  // $('.block__reviews').bxSlider({
+  //   minSlides: 3,
+  //   maxSlides: 3,
+  //   slideWidth: 440,
+  //   slideMargin: 200
+  // });
+
   $('.block__reviews').bxSlider({
     minSlides: 3,
     maxSlides: 3,
     slideWidth: 440,
-    slideMargin: 200
-  });
+    slideMargin: 200,
+    preventDefaultSwipeX: false,
+    preventDefaultSwipeY: false
+});
 
    $('.work-list').bxSlider({
     minSlides: 4,
@@ -33,3 +43,50 @@
     })
   }
   directions.addEventListener("mouseout",clousedirections)
+
+  let menu__link = document.querySelectorAll(".menu__link");
+  let targetId;
+  menu__link.forEach(function(element){
+    element.addEventListener("click",function(event){
+      event.preventDefault();
+      targetId=element.getAttribute("href");
+      document.querySelector(targetId).scrollIntoView({
+        behavior:"smooth",
+        block:"start"
+      })
+    })
+  })
+
+  // let slideup = document.querySelector("#slideup");
+  // document.addEventListener("mousemove",function(event){
+  //   if (event.pageY>790){
+  //     slideup.style.display="inline";
+  //   }
+  //   else{
+  //     slideup.style.display="none";
+  //   }
+  // })
+  let slideup = document.querySelector("#slideup");
+
+document.addEventListener("scroll", function() {
+    if (window.scrollY > 790) {
+        slideup.style.display = "inline";
+    } else {
+        slideup.style.display = "none";
+    }
+});
+
+let dragging = false;
+
+$('.course__list, .block__reviews').on('touchstart mousedown', function() {
+    dragging = false;
+}).on('touchmove mousemove', function() {
+    dragging = true;
+});
+
+$('.course__list a, .block__reviews a').on('click', function(e) {
+    if (dragging) {
+        e.preventDefault();
+    }
+});
+
